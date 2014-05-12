f4fb433dd18fad79359dce4bf3f40f82df7f6674
Tareq <tareq_hassan3336610@hotmail.com>
2014-05-13 01:28:26 +0600

    first commit of this project

---
+++ loop/loop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //commenting the while loop statement
+          /*  int countVariable = 0;
+            while (countVariable < 10)
+            {
+                Console.Write("For this iteration the value of countVariable is:{0}\n", countVariable);
+                countVariable++;
+            }*/
+            for (int counter=0; counter < 10; counter++)
+            {
+                Console.WriteLine("The value of the variable is:{0}\n", counter);
+            }
+        }
+    }
+}
